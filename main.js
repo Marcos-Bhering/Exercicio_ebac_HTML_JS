1f481b76e4ef0621a3623f06a93dea02643f1ccb
Marcos-Bhering <marcosschork@gmail.com>
2024-11-08 18:47:55 -0300

    correção exercicio

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
 const alerta = document.querySelector(".msg-alert");
-const num1 = document.querySelector("#num-1");
-const num2 = document.querySelector("#num-2");
 const button = document.querySelector("#btn");
 
 button.addEventListener("click", function (e) {
   e.preventDefault();
 
-  if (num1.value < num2.value) {
-    alerta.classList.remove("msg-alert-red");
-    alerta.classList.add("msg-alert-green");
-    alerta.innerHTML = "Tudo certo! O número 2 é maior!";
-  } else {
-    alerta.classList.add("msg-alert-red");
+  const num1 = document.querySelector("#num-1");
+  const num2 = document.querySelector("#num-2");
 
-    alerta.innerHTML = "Tente novamente! O número 1 é maior!";
+  if (num1 && num2) {
+    if (num1.value < num2.value) {
+      alerta.classList.remove("msg-alert-red");
+      alerta.classList.add("msg-alert-green");
+      alerta.innerHTML = "Tudo certo! O número 2 é maior!";
+    } else {
+      alerta.classList.remove("msg-alert-green");
+      alerta.classList.add("msg-alert-red");
+      alerta.innerHTML = "Tente novamente! O número 1 é maior!";
+    }
+
+    // Resetando os valores dos inputs
+    num1.value = "";
+    num2.value = "";
+  } else {
+    console.error("Um ou mais elementos não foram encontrados no DOM.");
   }
 });
